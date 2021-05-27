ef2668ff453144cc95c8f984f0b2920557fb15a1
richardsforu <praveen.somireddy@gmail.com>
2021-05-27 10:19:07 +0530

    delete logic implemented

--- 2-jQuery/jQuery-Form/app.js
+++ 2-jQuery/jQuery-Form/app.js
@@ -1,32 +1,78 @@
 $(function () {
 
-    var allPosts = [
-        { name: 'Pen', comment: 'Good' },
-        { name: 'Book', comment: 'jQuery' }
-    ];
+    var allPosts = [];
+
+    // api server url
+     var apiUrl ="http://49.207.3.199:3000/api/posts";
+
     // render all post
     var allPostDiv = $('#all-posts');
+
+    // load all posts from server side through XHR (AJAX) call
+
+    $.ajax(apiUrl,{
+        method:'GET',
+        success:function (posts){
+            allPosts=posts;
+            renderAllPosts();
+        },
+        error:function (e){
+            console.log(">>>>> Invalid Request <<<<<<");
+            console.log(e);
+        }
+    })
+
+
     function renderAllPosts() {
         allPostDiv.children().remove();
         allPosts.forEach(function (post) {
 
             var postTemplate = `
             <div class="list-group-item">
-                <div class="alert alert-info">
-                     Name: <span class="badge bg-primary">${post.name}</span>
+                <div class="alert alert-info" data-id="${post.id}">
+                     Name: <span class="badge bg-primary">${post.name}  </span> 
+                     <span class=''>
+                     <i class="bi bi-trash" id="post-delete"></i>
+                     <i class="bi bi-pencil-square"></i>
+                     </span>
+
                      <hr/>
-                      <p>${post.comment}</p>
+                     <span class="">${post.comment}  </span> 
+
                 </div>
 
             </div>
-            
-            
             `
             allPostDiv.append(postTemplate);
         })
 
     }
 
+
+    $('#all-posts').on('click','#post-delete',function(){
+        apiUrl+"/"+id
+       // console.log('... deleted');
+       var id= $(this).closest('div').data('id');
+       var deleteUrl=apiUrl+"/"+id
+      //  console.log(deleteUrl);
+      // console.log(id);
+
+       $.ajax(deleteUrl,{
+           method:'DELETE',
+           success:function(count){
+            console.log(count);
+           },
+           error:function(e){
+               console.log('Not Deleted because of '+e);
+           }
+       });
+
+      allPosts= allPosts.filter(function(post){
+           return post.id!==id;
+       });
+       renderAllPosts();
+
+    })
 
 
     $('#post-form').on('submit', function (e) {
@@ -51,9 +97,20 @@ $(function () {
             comment: commentField.val()
         }
 
-        console.log(newPost);
-        allPosts.push(newPost);
-        renderAllPosts();
+        // posting new post to api server though xhr (AJAX) call
+
+        $.ajax(apiUrl,{
+            method: 'POST',
+            contentType:'application/json',
+            data:JSON.stringify(newPost),
+            success:function(post){
+                allPosts.push(newPost);
+                renderAllPosts();
+            }
+        });
+
+        nameField.val('').focus();
+        commentField.val('');
 
     })
 })
